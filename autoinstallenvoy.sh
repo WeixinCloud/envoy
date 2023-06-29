fed18bf583f9722875e3c5745e54ccd6ad77b83b
lgq1205 <422895018@qq.com>
2023-06-29 10:30:39 +0800

    Update autoinstallenvoy.sh

--- autoinstallenvoy.sh
+++ autoinstallenvoy.sh
@@ -3,7 +3,8 @@
 # 变量
 
 
-port="9902:9902"
+port1="9902"
+port2="9902"
 hex_pub_key="0409F9DF311E5421A150DD7D161E4BC5C672179FAD1833FC076BB08FF356F35020CCEA490CE26775A52DC6EA718CC1AA600AED05FBF35E084A6632F6072DA9AD13"
 hex_pri_key="3945208F7B2144B13F36E38AC6D39F95889393692860B51A42FB81EF4DF7C5B8"
 config="envoy.yaml"
@@ -13,7 +14,7 @@ path="$(pwd)/${folder}"
 configpath="${path}/${config}"
 logpath="${path}/run.log"
 
-run="docker run -e HEX_PUB_KEY=${hex_pub_key} -e HEX_PRI_KEY=${hex_pri_key} -p ${port} -v ${path}:/home/envoy/custom ccr.ccs.tencentyun.com/weixincloud/wxsmgw:v1 /usr/local/bin/envoy -c /home/envoy/custom/${config} -l debug"
+run="docker run -e HEX_PUB_KEY=${hex_pub_key} -e HEX_PRI_KEY=${hex_pri_key} -p ${port1}:${port2} -v ${path}:/home/envoy/custom ccr.ccs.tencentyun.com/weixincloud/wxsmgw:v1 /usr/local/bin/envoy -c /home/envoy/custom/${config} -l debug"
 
 red='\e[31m'
 yellow='\e[33m'
@@ -30,15 +31,16 @@ msg() {
         local color=$red
         ;;
     ok)
-        local color=$blue
+        local color=$green
         ;;
     esac
+
 
     echo -e "[${color}$(date +'%T')] ${2}${none}"
 }
 
 msg ok "----------config begin----------"
-msg ok "${port}"
+msg ok "${port1}:${port2}"
 msg ok "${hex_pub_key}"
 msg ok "${hex_pri_key}"
 msg ok "${config}"
@@ -151,4 +153,4 @@ msg ok "Docker Run Begin log is ${logpath}"
 
 $run >> ${logpath} 2>&1
 
-msg ok "Docker Close
+msg ok "Docker Close"
